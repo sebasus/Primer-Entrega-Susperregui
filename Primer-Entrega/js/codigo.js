b0d5ea394751b75a02a554c32a76bda713978d3d
sebasus <susperregui.s@hotmail.com>
2022-07-28 21:30:34 -0300

    Primer-Entrega-Susperregui

--- Primer-Entrega/js/codigo.js
+++ Primer-Entrega/js/codigo.js
@@ -4,18 +4,20 @@ let valor = parseInt( prompt("Buen dia, por favor ingrese el monto total de su c
 while(valor < 0){
     valor = parseInt(prompt("Monto ingresado invalido, por favor vuelva a intentarlo")) 
 }   
-let cuotas = parseInt(prompt("Ahora ingrese el número de cuotas en las que desee hacer la compra:\n  3: 5% de interes.\n 6: 10% de interes. \n 12: 15%de interes. "))
+let cuotas = parseInt(prompt("Ahora ingrese el número de cuotas en las que desee hacer la compra:\n  3: 5% de interes.\n 6: 10% de interes. \n 12: 15% de interes. \n 24: 35% de interes."))
 
 let porcCuota 
 
-array = [5, 10, 15]
-
+let porcentaje = [5, 10, 15]
+porcentaje.push (35)
 switch (cuotas) {
-    case 3: porcCuota = array[0]
+    case 3: porcCuota = porcentaje[0]
     break;
-    case 6: porcCuota = array[1]
+    case 6: porcCuota = porcentaje[1]
     break;
-    case 12: porcCuota = array[2]
+    case 12: porcCuota = porcentaje[2]
+    break;
+    case 24: porcCuota = porcentaje[3]
     break;
     default:
         alert("Selecciono una cantidad de cuotas erronea, por favor recargue la pagina e inicie nuevamanete el proceso.")
